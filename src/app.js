6383b3f1928b5295fa38c1d1a9b34318fb9071e4
tbilbe <tbilbe@gmail.com>
2019-06-01 12:07:31 +0100

    refactor app.js

--- src/app.js
+++ src/app.js
@@ -6,15 +6,6 @@ const app = express();
 
 app.use(express.json());
 
-// app.get('*', (req, res) => {
-//   res.status(200).json({
-//     message: 'Hello World!'
-//   });
-// });
-
-// app.use(app.router);
-// routes.initialize(app);
-
 app.use('/artists', artistRouter);
 
 module.exports = app;
